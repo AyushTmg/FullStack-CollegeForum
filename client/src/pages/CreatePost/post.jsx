431c10592611768c94939006e92425981fe76306
AyushTmg <ayushtmg20040722@gmail.com>
2024-07-08 09:10:25 +0545

    Feat: Added The FrontEnd For Creating Post

---
+++ client/src/pages/CreatePost/post.jsx
@@ -0,0 +1,101 @@
+import './post.css'
+import InputField from '../../components/Common/InputField/InputField'
+import { useState } from 'react'
+import { userDetail } from '../../utils/userDetail/userDetail';
+import api from '../../api/api';
+import ToastMessage from '../../utils/toaster/toaster';
+import { isAxiosError } from "axios";
+import { useNavigate } from 'react-router-dom';
+
+const SEMESTER_CHOICES = [
+    '1st Semester',
+    '2nd Semester',
+    '3rd Semester',
+    '4th Semester',
+    '5th Semester',
+    '6th Semester',
+    '7th Semester',
+    '8th Semester',
+];
+
+export default function CreatePost() {
+    const navigate = useNavigate()
+
+    const userData = userDetail()
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [semester, setSemester] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await api.post(
+                'api/forum/questions/',
+                {
+                    title: title,
+                    description: description,
+                    semester: semester,
+                    user_id: userData.user_id
+                }
+            )
+            const response = res.data;
+
+            if (response.success) {
+                ToastMessage.success(response.message)
+                navigate('/login')
+            }
+        }
+        catch (error) {
+            if (isAxiosError(error)) {
+                ToastMessage.error(error.response.data.message);
+            } else {
+                ToastMessage.error("An unexpected error occurred.", error);
+                console.log(error)
+            }
+        }
+    }
+
+    return (
+        <>
+            <form method="post" onSubmit={handleSubmit}>
+                <InputField
+                    type="text"
+                    name="title"
+                    placeholder="Enter the title"
+                    value={title}
+                    change={(e) => {
+                        setTitle(e.target.value)
+                    }}
+                />
+                <InputField
+                    type="text"
+                    name="description"
+                    placeholder="Enter the description"
+                    value={description}
+                    change={(e) => {
+                        setDescription(e.target.value)
+                    }}
+                />
+                <div>
+                    <label htmlFor="semester">Select Semester</label>
+                    <select
+                        id="semester"
+                        name="semester"
+                        value={semester}
+                        onChange={(e) => setSemester(e.target.value)}
+                    >
+                        <option value="">Select a semester</option>
+                        {SEMESTER_CHOICES.map((semesterChoice) => (
+                            <option key={semesterChoice} value={semesterChoice}>
+                                {semesterChoice}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <button type="submit" >Submit</button>
+            </form>
+        </>
+    )
+}
--- client/src/routes/RouteLayout.jsx
+++ client/src/routes/RouteLayout.jsx
@@ -9,6 +9,7 @@ import { ChangePassword } from "../pages/ChangePassword/changePassword";
 import ForgotPassword from "../pages/ForgotPassword/forgotPassword";
 import { ResetPassword } from "../pages/ResetPassword/resetPassword";
 import DetailPage from "../pages/DetailPage/detailPage";
+import CreatePost from "../pages/CreatePost/post";
 
 
 function RegisterAndLogout() {
@@ -33,32 +34,43 @@ export default function RouteLayout() {
                         <RegisterAndLogout />
                     </PublicRoute>
                 } />
+
                 <Route path="/change-password" element={
                     <ProtectedRoute>
                         <ChangePassword />
                     </ProtectedRoute>
                 } />
+
                 <Route path="/forgot-password" element={
                     <PublicRoute>
                         <ForgotPassword />
                     </PublicRoute>
                 } />
+
                 <Route path="/reset-password/:uid/:token" element={
                     <PublicRoute>
                         <ResetPassword />
                     </PublicRoute>
                 } />
+
                 <Route path="/" element={
                     <ProtectedRoute>
                         <Home />
                     </ProtectedRoute>}>
                 </Route>
+
                 <Route path="/question/:id" element={
                     <ProtectedRoute>
                         <DetailPage />
                     </ProtectedRoute>
                 }
                 />
+
+                <Route path="/create-post" element={
+                    <ProtectedRoute>
+                        <CreatePost />
+                    </ProtectedRoute>}>
+                </Route>
 
                 {/* ! If the Route Other than the specified one are mapped this NotFound Component Will be Called */}
                 <Route path="*" element={<NotFound />} />
