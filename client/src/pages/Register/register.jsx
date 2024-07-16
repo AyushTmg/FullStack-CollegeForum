49a943ca6a107857f82a7a49e0ede549ebbe3db7
AyushTmg <ayushtmg20040722@gmail.com>
2024-07-16 20:50:42 +0545

    Feat: Styling For UserRegistration Is Added With Specific Input Field For User Registration

--- client/src/pages/Register/register.jsx
+++ client/src/pages/Register/register.jsx
@@ -1,11 +1,11 @@
 import "./register.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import InputField from "../../components/Common/InputField/InputField";
 import { Link } from 'react-router-dom';
 import ToastMessage from "../../utils/toaster/toaster";
 import { isAxiosError } from "axios";
 import { userRegister } from "../../services/Authentication/auth";
+import RegisterInputField from "../../components/Common/InputField/RegisterInputFields";
 
 export default function Register() {
     const [firstName, setFirstName] = useState('');
@@ -52,74 +52,133 @@ export default function Register() {
 
     return (
         <>
-            <form onSubmit={handleSubmit} className="register-form">
-                <h1>User Register</h1>
+            <section style={{ height: '100vh' }}>
+                <div className="container">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col my-4">
+                            <div className="row g-0">
+                                <div className="col-xl-5 d-none d-xl-block">
+                                    <img src="/images/register.png" alt="Sample" className="img-fluid" style={{ borderTopLeftRadius: '.25rem', borderBottomLeftRadius: '.25rem' }} />
+                                </div>
 
-                <InputField
-                    type="text"
-                    name="firstName"
-                    placeholder="Enter your first name"
-                    value={firstName}
-                    className="form-input"
-                    change={(e) => setFirstName(e.target.value)}
-                    error={error.first_name}
-                />
 
-                <InputField
-                    type="text"
-                    name="lastName"
-                    placeholder="Enter your last name"
-                    value={lastName}
-                    className="form-input"
-                    change={(e) => setLastName(e.target.value)}
-                    error={error.last_name}
-                />
+                                <div className="col-xl-7">
 
-                <InputField
-                    type="text"
-                    name="username"
-                    placeholder="Enter your username"
-                    value={username}
-                    className="form-input"
-                    change={(e) => setUsername(e.target.value)}
-                    error={error.username}
-                />
+                                    <div className="p-md-5 text-white">
+                                        <h3 className="mb-5 text-uppercase">Registration Form</h3>
+                                        <form onSubmit={handleSubmit}>
+                                            <div className="row">
+                                                <div className="col-md-6 mb-4">
+                                                    <div data-mdb-input-init className="form-outline">
+                                                        <RegisterInputField
+                                                            type='text'
+                                                            name='firstname'
+                                                            label="First Name"
+                                                            placeholder="Enter your first name"
+                                                            value={firstName}
+                                                            change={(e) => setFirstName(e.target.value)}
+                                                            error={error.first_name}
+                                                        />
+                                                    </div>
+                                                </div>
+                                                <div className="col-md-6 mb-4">
+                                                    <div data-mdb-input-init className="form-outline">
+                                                        <RegisterInputField
+                                                            type='text'
+                                                            name='lastname'
+                                                            label="Last Name"
+                                                            placeholder="Enter your last name"
+                                                            value={lastName}
+                                                            change={(e) => setLastName(e.target.value)}
+                                                            error={error.last_name}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </div>
 
-                <InputField
-                    type="email"
-                    name="email"
-                    placeholder="Enter your email"
-                    value={email}
-                    className="form-input"
-                    change={(e) => setEmail(e.target.value)}
-                    error={error.email}
-                />
+                                            <div className="row">
+                                                <div className="col-md-6 mb-4">
+                                                    <div data-mdb-input-init className="form-outline">
+                                                        <RegisterInputField
+                                                            type='text'
+                                                            name="username"
+                                                            label="Username"
+                                                            placeholder="Enter your username"
+                                                            value={username}
+                                                            change={(e) => setUsername(e.target.value)}
+                                                            error={error.username}
+                                                        />
+                                                    </div>
+                                                </div>
+                                                <div className="col-md-6 mb-4">
+                                                    <div data-mdb-input-init className="form-outline">
+                                                        <RegisterInputField
+                                                            type="email"
+                                                            name="email"
+                                                            label="Email"
+                                                            placeholder="Enter your email"
+                                                            value={email}
+                                                            change={(e) => setEmail(e.target.value)}
+                                                            error={error.email}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </div>
 
-                <InputField
-                    type="password"
-                    name="password"
-                    placeholder="Enter your password"
-                    value={password}
-                    className="form-input"
-                    change={(e) => setPassword(e.target.value)}
-                    error={error.password}
-                />
+                                            <div data-mdb-input-init className="form-outline mb-4">
+                                                <RegisterInputField
+                                                    type="password"
+                                                    name="password"
+                                                    label="Password"
+                                                    placeholder="Enter your password"
+                                                    value={password}
+                                                    change={(e) => setPassword(e.target.value)}
+                                                    error={error.password}
+                                                />
+                                            </div>
 
-                <InputField
-                    type="password"
-                    name="passwordConfirmation"
-                    placeholder="Enter your password confirmation"
-                    value={passwordConfirmation}
-                    className="form-input"
-                    change={(e) => setPasswordConfirmation(e.target.value)}
-                    error={error.password_confirmation}
-                />
+                                            <div data-mdb-input-init className="form-outline mb-4">
+                                                <RegisterInputField
+                                                    type="password"
+                                                    label="Repeat Password "
+                                                    name="passwordConfirmation"
+                                                    placeholder="Repeat your password"
+                                                    value={passwordConfirmation}
+                                                    change={(e) => setPasswordConfirmation(e.target.value)}
+                                                    error={error.password_confirmation}
+                                                />
+                                            </div>
 
-                <button type="submit" className="form-button">
-                    Register
-                </button>
-                <div>Already have an account? <Link to="/login">Login</Link></div>
-            </form>
+                                            <div className="d-flex justify-content-center ">
+                                                <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary btn-lg ms-2">Register</button>
+
+                                            </div>
+                                            <div className="fw-bold d-flex justify-content-center  pt-3">
+                                                <div>
+                                                    Already have an account? <Link to="/login" className="link-danger">
+                                                        Login
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                            {/* <div className="d-flex justify-content-between align-items-center pt-3">
+                                                <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary btn-lg ms-2">Register</button>
+
+                                                <div className="fw-bold">
+                                                    Already have an account? <Link to="/login" className="link-danger">
+                                                        Login
+                                                    </Link>
+                                                </div>
+                                            </div> */}
+
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
         </>
     );
 }
