f2bbae9ceeaad660c794445f2cef748b349946c4
AyushTmg <ayushtmg20040722@gmail.com>
2024-07-20 18:36:18 +0545

    Feat: Added New Component Structure Component For Structuing the Web Layout

--- client/src/pages/Home/home.jsx
+++ client/src/pages/Home/home.jsx
@@ -3,12 +3,13 @@ import { userDetail } from '../../utils/userDetail/userDetail';
 import { useEffect, useState } from 'react';
 import QuestionCard from '../../components/Question/question';
 import { fetchQuestionList } from '../../services/Forum/forum';
-import Header from '../../components/Header/header';
-
+import { Loading } from '../../components/Common/Loading/loading';
+import "./home.css"
 
 export default function Home() {
     const userData = userDetail();
     const [questions, setQuestions] = useState([]);
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const fetchQuestions = async () => {
@@ -17,6 +18,7 @@ export default function Home() {
                 if (response.success) {
                     setQuestions(response.data);
                 }
+                setLoading(false)
             } catch (error) {
                 if (axios.isAxiosError(error)) {
                     ToastMessage.error(error.response.data.message || "An error occurred while fetching questions.");
@@ -32,25 +34,28 @@ export default function Home() {
 
     return (
         <>
-            <Header />
-            {/*For User Logout */}
-            <div className='d-flex justify-content-center'>
-                <div>Welcome {userData.username}</div>
-            </div>
+            {loading ?
+                <Loading />
+                :
+                <div>
+                    <div className="col-lg-12">
+                        <div className='questions-container'>
+                            {questions ? questions.map((question) => (
+                                <QuestionCard
+                                    key={question.id}
+                                    title={question.title}
+                                    user={question.user}
+                                    likes={question.likes}
+                                    timeStamp={question.time_stamp}
+                                    semester={question.semester}
+                                    id={question.id}
+                                />
+                            )) : <div>No Data</div>}
+                        </div>
 
-            <div className='questions-container'>
-                {questions ? questions.map((question) => (
-                    <QuestionCard
-                        key={question.id}
-                        title={question.title}
-                        user={question.user}
-                        likes={question.likes}
-                        timeStamp={question.time_stamp}
-                        semester={question.semester}
-                        id={question.id}
-                    />
-                )) : <div>No Data</div>}
-            </div>
+                    </div >
+                </div >
+            }
 
 
         </>
